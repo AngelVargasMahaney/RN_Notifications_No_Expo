0be91ea7a3f9bde0dcc2b701ae07efc6e0a48dbf
AngelVargasMahaney <angelvgmh@gmail.com>
2022-12-21 10:35:50 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>App Created</Text>
+      <Text>App Created</Text>
+      <Text>App Created</Text>
+    </View>
+  )
+}
+
+export default App
